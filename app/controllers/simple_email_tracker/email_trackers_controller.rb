f8e4faca2143426ee6c24fb5c6036ea6cf7b703e
halida <linjunhalida@gmail.com>
2012-12-04 20:18:58 +0800

    fix bug

--- app/controllers/simple_email_tracker/email_trackers_controller.rb
+++ app/controllers/simple_email_tracker/email_trackers_controller.rb
@@ -4,10 +4,11 @@ module SimpleEmailTracker
     def show
       v = Visit.find_by_uuid params[:uuid]
       v.visit_by request
-      send_file(
-           File.expand_path("../../../assets/images/t.gif", __FILE__),
+      send_data(
+           File.read(File.expand_path("../../../assets/images/t.gif", __FILE__)),
            filename: "t.gif",
-           type: "image/pdf")
+           disposition: 'inline',
+           type: "image/gif")
     end
 
   end
--- lib/simple_email_tracker/view_helper.rb
+++ lib/simple_email_tracker/view_helper.rb
@@ -2,7 +2,7 @@ module SimpleEmailTracker
   module ViewHelper
     def set_email_tracker *args
       et = SimpleEmailTracker::Visit.get_by_key args
-      "#{root_url}email_trackers/#{et.uuid}/t.gif"
+      image_tag "#{root_url}email_trackers/#{et.uuid}/t.gif"
     end
   end
 end
