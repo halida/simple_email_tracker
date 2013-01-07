fbe3a5d4fc140b565ca3e7302147c5f02a456738
halida <linjunhalida@gmail.com>
2013-01-07 10:35:34 +0800

    try to fix email tracker issue

--- app/controllers/simple_email_tracker/email_trackers_controller.rb
+++ app/controllers/simple_email_tracker/email_trackers_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module SimpleEmailTracker
   class EmailTrackersController < ::ApplicationController
 
@@ -5,7 +6,7 @@ module SimpleEmailTracker
       v = Visit.find_by_uuid params[:uuid]
       v.visit_by request
       send_data(
-           File.read(File.expand_path("../../../assets/images/t.gif", __FILE__)),
+           "GIF89a\u000F\u0000\u000F\u0000\x80\u0000\u0000\xFF\xFF\xFF\u0000\u0000\u0000!\xF9\u0004\u0000\u0000\u0000\u0000\u0000,\u0000\u0000\u0000\u0000\u000F\u0000\u000F\u0000\u0000\u0002\r\x84\x8F\xA9\xCB\xED\u000F\xA3\x9C\xB4ڋg\u0001\u0000;",
            filename: "t.gif",
            disposition: 'inline',
            type: "image/gif")
